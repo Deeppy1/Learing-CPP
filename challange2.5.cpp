d76134170a72a559ed2719e5c79b0176ecead366
Deeppy1 <161700797+Deeppy1@users.noreply.github.com>
2025-05-23 10:48:42 +0000

    2.5

--- challange2.5.cpp
+++ challange2.5.cpp
@@ -2,16 +2,19 @@
 #include<string>
 #include<vector>
 int main(){
+    int number;
     std::vector<std::string> names;
     std::string name;
-    for(int i = 0; i < 3; i++){
-        std::cout << "Enter name " << i +1 << " : "
+    std::cout << "How many names? "; 
+    std::cin >> number;
+    for(int i = 0; i < number; i++){
+        std::cout << "Enter name " << i + 1 << " : ";
         std::cin >> name;
         names.push_back(name);
     }
     std::cout << "\n You entered: \n" << std::endl;
-    for(int i = 0; i < name.size(); i++){
-        std::cout << names[i] << sdt::endl;
+    for(int i = 0; i < number; i++){
+        std::cout << names[i] << std::endl;
     }
-    return 0
+return 0;
 }
