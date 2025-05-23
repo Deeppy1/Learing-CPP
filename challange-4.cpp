cfa98bfde14af2e8f51b3b0c3931aa922caa3efc
Deeppy1 <161700797+Deeppy1@users.noreply.github.com>
2025-05-23 12:48:06 +0000

    challange 4

--- challange-4.cpp
+++ challange-4.cpp
@@ -21,11 +21,23 @@ int main() {//main
         std::getline(std::cin, temp.name);
         std::cout << "Enter Age: ";
         std::cin >> temp.age;
+        if(temp.age < 5 || temp.age > 100){
+            std::cout << "Invalid Age Please enter an age between 5 and 100: ";
+            std::cin >> temp.age;
+        }
         std::cout << "Enter Grade (A,B,C,D,E,F): ";
         std::cin >> temp.grade;
+        temp.grade = toupper(temp.grade);
+        while (!(temp.grade == 'A' || temp.grade == 'B' || temp.grade == 'C' || temp.grade == 'D' || temp.grade == 'E' || temp.grade == 'F')) {
+            std::cout << "Invalid Grade Please Enter (A-F): ";
+            std::cin >> temp.grade;
+            temp.grade = toupper(temp.grade);
+        }
+
         std::cin.ignore();
         students.push_back(temp);
     }
+
     std::cout << "\n List Of Students" << std::endl;
     std::cout << "-------------------------" << std::endl;
     for(int i = 0; i < students.size(); i++) {
--- challange2.5.cpp
+++ challange2.5.cpp
@@ -1,20 +1,21 @@
+//lib
 #include<iostream>
 #include<string>
 #include<vector>
-int main(){
-    int number;
-    std::vector<std::string> names;
-    std::string name;
-    std::cout << "How many names? "; 
-    std::cin >> number;
-    for(int i = 0; i < number; i++){
-        std::cout << "Enter name " << i +1 << " : ";
-        std::cin >> name;
-        names.push_back(name);
+int main(){//main
+    int number;//define variable
+    std::vector<std::string> names;//define vector 
+    std::string name;//define variable string
+    std::cout << "How many names? "; //ask how many
+    std::cin >> number;//get the input
+    for(int i = 0; i < number; i++){//for loop for i in how ever many
+        std::cout << "Enter name " << i +1 << " : ";//ask for the name
+        std::cin >> name;//get input
+        names.push_back(name);//add it to the vector
     }
-    std::cout << "\n You entered: \n" << std::endl;
-    for(int i = 0; i < number; i++){
-        std::cout << names[i] << std::endl;
+    std::cout << "\n You entered: \n" << std::endl;//state name title
+    for(int i = 0; i < number; i++){//for loop
+        std::cout << names[i] << std::endl;//display names
     }
-return 0;
+return 0;//exit and state succses
 }
