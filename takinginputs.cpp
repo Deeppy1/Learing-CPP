71f181262cafbeac2a7da200ed72d056a98a5d5c
Deeppy1 <161700797+Deeppy1@users.noreply.github.com>
2025-05-23 08:02:55 +0000

    comments

--- takinginputs.cpp
+++ takinginputs.cpp
@@ -2,9 +2,9 @@
 #include<iostream>
 #include<string>
 int main() { //Main Function
-    std::string name;
-    std::cout << "Enter your name ";
-    std::getline(std::cin, name);
-    std::cout << "Hello " << name << "!" << std::endl;
-    return 0;
+    std::string name;// Define Variable
+    std::cout << "Enter your name ";// Print the enter your name line
+    std::getline(std::cin, name);// Get the input
+    std::cout << "Hello " << name << "!" << std::endl;// Print the variable with other bits
+    return 0;// Return Succses
 }
