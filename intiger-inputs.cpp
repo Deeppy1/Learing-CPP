0271493ec03e8a39eb571d75a744edec612c6b4d
Deeppy1 <161700797+Deeppy1@users.noreply.github.com>
2025-05-23 08:22:57 +0000

    comments

--- intiger-inputs.cpp
+++ intiger-inputs.cpp
@@ -1,12 +1,13 @@
+//Starting Libarys
 #include<iostream>
 #include<string>
-int main(){
-    int num1, num2;
-    std::cout << "Enter first number: ";
-    std::cin >> num1;
-    std::cout << "Enter seccond number: ";
-    std::cin >> num2;
-    int sum = num1 + num2;
-    std::cout << "Total is: " << sum << std::endl;
-    return 0;
+int main(){ //Main function
+    int num1, num2;// Declare variables
+    std::cout << "Enter first number: ";// First input
+    std::cin >> num1;//Get number
+    std::cout << "Enter seccond number: ";// Seccond input
+    std::cin >> num2;// Get number 2
+    int sum = num1 + num2;// Define variable and add numbers
+    std::cout << "Total is: " << sum << std::endl;// Print output
+    return 0;// Return Succses
 }
